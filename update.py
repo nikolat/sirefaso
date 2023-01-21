7843c16eb0eb9a52c2b7e3788426d44478cc727c
Don <evidence@384.jp>
2023-01-21 20:15:49 +0900

    not null

--- update.py
+++ update.py
@@ -57,7 +57,7 @@ class GitHubDauCrawler(crawler.GitHubApiCrawler):
 					'author': item['owner']['login'],
 					'author_url': item['owner']['html_url'],
 					'author_avatar': item['owner']['avatar_url'],
-					'content_text': item['description'],
+					'content_text': item['description'] if 'description' in item else '',
 					'tags': item['topics'],
 					'html_url': item['html_url'],
 					'created_at_time': item['created_at'],
